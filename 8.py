47adbd2099241afa65e1c41cbb2c36406e7863bd
Krisl1107 <Klagoda_zeya@mail.ru>
2025-03-24 09:28:04 +0700

    Update 8.py

--- 8.py
+++ 8.py
@@ -1,18 +1,13 @@
-s = int(input())
-
+k = int(input())
 numbers = []
-for h in range (0, 10):
-    numbers.append (h)
-
-for a in range (10, 100):
-    numbers.append (a // 10)
+for c in range (0, 10):
+    numbers.append (c)
+for b in range (10, 100):
+    numbers.append (b // 10)
+    numbers.append (b % 10)
+for a in range (100, 201):
+    numbers.append ((a // 10) // 10)
+    numbers.append ((a // 10) % 10)
     numbers.append (a % 10)
-
-for x in range (100, 201):
-    numbers.append ((x // 10) // 10)
-    numbers.append ((x // 10) % 10)
-    numbers.append (x % 10)
-
-k = numbers[s-1]
-
-print(k)
+t = numbers[k-1]
+print(t)
